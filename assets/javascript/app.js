4b7e0da8c7a9bd0dc9fbb351d18b9a843a6f048e
CPUHobbes <ericms@utexas.edu>
2016-09-06 15:54:07 -0500

    Fixed score timing delay

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,29 +9,31 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-//NEW
 var playerWarrior="";
 var playerName="Noob Saibot";
 var hasSelectedFighter = false;
 
-//NEW
-
 var ANI_DIR = "assets/images/characters/animation/";
-
 
 var playerID=0;
 var openPlayerOne=true;
 var openPlayerTwo=true;
-//var playerName="";
 var canJoin = true;
 
 var numWins = 0;
 var numLosses = 0;
 
+var winsP1=0;
+var winsP2=0;
+var lossesP1=0;
+var lossesP2=0;
+var winnerText="";
+
 var choiceOne;
 var choiceTwo;
 
-var timer;
+var gameTimer;
+var resultsTimer;
 
 
 $(document).ready(function(){
@@ -102,7 +104,6 @@ $(document).on("click", ".weaponImage", function(){
 
 
 function getCurrentPlayers(){
-
 
 	database.ref().child("player").on("child_added", function(snapshot) {
 
@@ -190,7 +191,6 @@ function addUser(currentPlayer, seatNum, warrior){
 		connectDB(playerName);
 		disconnectDB(playerID);
 		canJoin = false;
-
 	}
 
 	else if(!canJoin && playerID>0){
@@ -200,15 +200,12 @@ function addUser(currentPlayer, seatNum, warrior){
 	else{
 		alert("Game Full!");
 	}
-	
 }
-
 
 function writeChat(chat){
 	var lineObj = {line: chat};
 
-	firebase.database().ref('chat/').update(lineObj);
-
+	database.ref('chat/').update(lineObj);
 }
 
 function readChat(){
@@ -219,14 +216,12 @@ function readChat(){
 	  		var chatObject = snapshot.val();
 
 	  		if(chatObject !==""){
-	  			$("#chatBox").append('\n'+chatObject);
+	  			$("#chatBox").append(chatObject+'\n');
 	  			$("#chatBox").scrollTop($("#chatBox")[0].scrollHeight);
 	  		}
   		}
 	});
-
 }
-
 
 function disconnectDB(num){
 	database.ref().child("chat").onDisconnect().update({line: playerName+" has left the game!"});
@@ -245,19 +240,9 @@ function clearChat(){
 	database.ref().child("chat").set({line:""});
 }
 
-function writePlayerDiv(){
-
-	for(var i=1;i<3;++i){
-		var tempDiv = $("#player"+i+"Score");
-		var score = "<p>Wins:<span id=\"wins"+i+"\">"+numWins+"</span>Losses:<span id=\"losses"+i+"\">"+numLosses+"</span></p>";
-		tempDiv.append(score);
-	}
-}
-
 function playerChoice(item){
 	var tempObj =  {choice: item};
 	database.ref().child("player").child(playerID).update(tempObj);
-
 }
 
 function checkResult(){
@@ -276,70 +261,64 @@ function checkResult(){
 				
 				$("#playerRPS1").attr("alt", "rock").attr("width", "100").attr("src", "assets/images/left_R.gif");
 				$("#playerRPS2").attr("alt", "scissors").attr("width", "100").attr("src", "assets/images/right_S.gif");
-
-				$("#textResult").append("<h3>Player 1 Wins!</h3>");
-				console.log(pOneChoice, pTwoChoice);
+				winnerText="Player 1 Wins!";
 				results(1);
 			}
 			else if ((pOneChoice === "rock") && (pTwoChoice === "paper")){
 				$("#playerRPS1").attr("alt", "rock").attr("width", "100").attr("src", "assets/images/left_R.gif");
 				$("#playerRPS2").attr("alt", "paper").attr("width", "100").attr("src", "assets/images/right_P.gif");
-				$("#textResult").append("<h3>Player 2 Wins!</h3>");
-				console.log(pOneChoice, pTwoChoice);
+				winnerText="Player 2 Wins!";
+				
 				results(2);
 			}
 			else if ((pOneChoice === "scissors") && (pTwoChoice === "rock")){
 				$("#playerRPS1").attr("alt", "scissors").attr("width", "100").attr("src", "assets/images/left_S.gif");
 				$("#playerRPS2").attr("alt", "rock").attr("width", "100").attr("src", "assets/images/right_R.gif");
-				$("#textResult").append("<h3>Player 2 Wins!</h3>");
-				console.log(pOneChoice, pTwoChoice);
+				winnerText="Player 2 Wins!";
+				
 				results(2);
 			}
 			else if ((pOneChoice === "scissors") && (pTwoChoice === "paper")){
 				$("#playerRPS1").attr("alt", "scissors").attr("width", "100").attr("src", "assets/images/left_S.gif");
 				$("#playerRPS2").attr("alt", "paper").attr("width", "100").attr("src", "assets/images/right_P.gif");
-				$("#textResult").append("<h3>Player 1 Wins!</h3>");
-				console.log(pOneChoice, pTwoChoice);
+				winnerText="Player 1 Wins!";
+				
 				results(1);
 			}
 			else if ((pOneChoice === "paper") && (pTwoChoice === "rock")){
 				$("#playerRPS1").attr("alt", "paper").attr("width", "100").attr("src", "assets/images/left_P.gif");
 				$("#playerRPS2").attr("alt", "rock").attr("width", "100").attr("src", "assets/images/right_R.gif");
-				$("#textResult").append("<h3>Player 1 Wins!</h3>");
-				console.log(pOneChoice, pTwoChoice);
+				winnerText="Player 1 Wins!";
+				
 				results(1);
 			}
 			else if ((pOneChoice === "paper") && (pTwoChoice === "scissors")){
 				$("#playerRPS1").attr("alt", "paper").attr("width", "100").attr("src", "assets/images/left_P.gif");
 				$("#playerRPS2").attr("alt", "scissors").attr("width", "100").attr("src", "assets/images/right_S.gif");
-				$("#textResult").append("<h3>Player 2 Wins!</h3>");
-				console.log(pOneChoice, pTwoChoice);
+				winnerText="Player 2 Wins!";
+				
 				results(2);
 			}
 			else if ((pOneChoice === pTwoChoice) && (pTwoChoice === "paper")){
 				$("#playerRPS1").attr("alt", "paper").attr("width", "100").attr("src", "assets/images/left_P.gif");
 				$("#playerRPS2").attr("alt", "paper").attr("width", "100").attr("src", "assets/images/right_P.gif");
-				$("#textResult").append("<h3>Its a Tie!</h3>");
-				console.log(pOneChoice, pTwoChoice);
+				winnerText="It's a Tie!";
+				
 				results(0);
 			}
 			else if ((pOneChoice === pTwoChoice) && (pTwoChoice === "scissors")){
 				$("#playerRPS1").attr("alt", "scissors").attr("width", "100").attr("src", "assets/images/left_S.gif");
 				$("#playerRPS2").attr("alt", "scissors").attr("width", "100").attr("src", "assets/images/right_S.gif");
-				$("#textResult").append("<h3>Its a Tie!</h3>");
-				console.log(pOneChoice, pTwoChoice);
+				winnerText="It's a Tie!";
 				results(0);
 			}
 			else if ((pOneChoice === pTwoChoice) && (pTwoChoice === "rock")){
 				$("#playerRPS1").attr("alt", "rock").attr("width", "100").attr("src", "assets/images/left_R.gif");
 				$("#playerRPS2").attr("alt", "rock").attr("width", "100").attr("src", "assets/images/right_R.gif");
-				$("#textResult").append("<h3>Its a Tie!</h3>");
-				console.log(pOneChoice, pTwoChoice);
-				results(0);
+				winnerText="It's a Tie!";
 			}
 		}
-	});
-		
+	});	
 }
 
 function results(playerNum){
@@ -351,19 +330,14 @@ function results(playerNum){
 
 		numLosses+=1;
 	}
-	// else{ //TIE
-		
-	// }
-	console.log(numWins, numLosses);
 	updateScoreDB();
 	newGameTimer();
-
 }
+
 function clearResults(){
 	database.ref().child("player").child("1").child("choice").remove();
 	database.ref().child("player").child("2").child("choice").remove();
 }
-
 
 function updateScoreDB(){
 	if(playerID!==0){
@@ -372,53 +346,58 @@ function updateScoreDB(){
 		database.ref().child("player").child(playerID).update(tempObj);
 		database.ref().child("player").child(playerID).update(tempObj2);
 	}
-
-	
 }
 
 function getScore(){
 
 	database.ref().child("player").on("child_changed", function(snapshot) {
 		if(snapshot.val().seat === 1){
-			$("#scorePlayer1").html("Wins: "+ snapshot.val().wins+" Losses: "+snapshot.val().losses);
-			// $("#winsPlayer1").html(snapshot.val().wins);
+			winsP1 = snapshot.val().wins;
+			lossesP1 = snapshot.val().losses;
+			// var ratioP1 = 100-(winsP1/lossesP1*50);
+			// $("#playerHealth1").css({"width":ratioP1+"%"});
+			// $("#scorePlayer1").html("Wins: "+winsP1+" Losses: "+lossesP1);
+			// console.log("p1 rat-",ratioP1);
+
 		}
 		else if (snapshot.val().seat === 2){
-			$("#scorePlayer2").html("Wins: "+ snapshot.val().wins+" Losses: "+snapshot.val().losses);
+			winsP2 = snapshot.val().wins;
+			lossesP2 = snapshot.val().losses;
+			// var ratioP2 = 100-(winsP2/lossesP2*50);
+			// $("#playerHealth2").css({"width":ratioP2+"%"});
+			// $("#scorePlayer2").html("Wins: "+winsP2+" Losses: "+lossesP2);
+			// console.log("p2 rat-",ratioP2);
 		}
+
 	});
 }
 
 function newGameTimer(){
-	timer = setTimeout(function() {
-		$("#fightBox").css({"visibility":"hidden"});
+	resultsTimer = setTimeout(function(){
+
+		resultsDelay();
+	}
+	,2000);
+
+
+	gameTimer = setTimeout(function() {
+		//Replacing SRC. If not, gifs will not reload animation.
 		$("#playerRPS1").attr("src", "assets/images/characters/animation/empty.png");
 		$("#playerRPS1").attr("alt", "dummyIMG");
 		$("#playerRPS1").attr("width", "1");
-
 		$("#playerRPS2").attr("src", "assets/images/characters/animation/empty.png");
 		$("#playerRPS2").attr("alt", "dummyIMG");
 		$("#playerRPS2").attr("width", "1");
 
+		$("#fightBox").css({"visibility":"hidden"});
 		$("#textResult").empty();
 		showWeapons();
+		
 	}
-	, 4000);
-}
-
-function sitInChair(){
-	showWeapons();
-
+	, 5000);
 }
 
 function leaveChair(seatNum){
-	// if(playerID===0){
-	// 	$("#joinBlock"+seatNum).append("<input type=\"text\" class=\"nameTextBox\" id=\"playerName"+seatNum+"\" value=\"Enter Name\" size=\"15\" />"+ 
-	// 									"<button class=\"joinButton\" data-player=\""+seatNum+"\">Join</button>");
-	// }
-	// $("#playerScore"+seatNum).empty();
-	// $("#playerNameBox"+seatNum).empty();
-	// $("#playerNameBox"+seatNum).append("<h2>Waiting for Player</h2>");
 	if(seatNum===1){
 		openPlayerOne=true;
 	}
@@ -435,6 +414,18 @@ function showWeapons(){
 		var paper = "<div class=\"text-center\"><img class=\"weaponImage\" data-item=\"paper\" width=\"80\" src=\"./assets/images/weapons/paper.png\"></div>";
 		var scissors = "<div class=\"text-center\"><img class=\"weaponImage\" data-item=\"scissors\" width=\"80\" src=\"./assets/images/weapons/scissors.png\"></div>";
 		imgDiv.append(rock,paper,scissors);
-	//}
+}
+
+function resultsDelay(){
+	$("#textResult").append("<h3 class=\"endText\">"+winnerText+"</h3>");
+	var ratioP1 = (lossesP1/(lossesP1+winsP1))*100;
+	$("#playerHealth1").css({"width":ratioP1+"%"});
+	$("#scorePlayer1").html("Wins: "+winsP1+" Losses: "+lossesP1);
+
+	var ratioP2 = (lossesP2/(lossesP2+winsP2))*100;
+	$("#playerHealth2").css({"width":ratioP2+"%"});
+	$("#scorePlayer2").html("Wins: "+winsP2+" Losses: "+lossesP2);
+
+
 
 }
