ab5ff47083e51b282598a324e8ef68402778d475
CPUHobbes <ericms@utexas.edu>
2016-09-07 15:45:59 -0500

    Completed comments, minor bug fixes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
 // Initialize Firebase
-
 var config = {
     apiKey: "AIzaSyD1kC59azlbLde4dl0T-8Z2qdm9ifRCjHo",
     authDomain: "rps-multiplayer-616fd.firebaseapp.com",
@@ -9,41 +8,56 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-var playerWarrior="";
-var fighterName1 ="";
-var fighterName2 ="";
+/*
+ *Global Variables
+ */
 
-var playerName="Noob Saibot";
-var hasSelectedFighter = false;
-
+//Static variables
 var ANI_DIR = "assets/images/characters/animation/";
 var MUSIC_DIR = "assets/audio/music/";
 var MUSIC_LIST = ["courtyard.mp3", "palace_gates.mp3", "pit.mp3", "throne_room.mp3", "warrior_shrine.mp3"];
 
+//Player variables
+var playerWarrior="";
+var fighterName1 ="";
+var fighterName2 ="";
+var playerName="Noob Saibot"; //Default Name
 var playerID=0;
+var hasSelectedFighter = false;
+
+//Database variables
 var openPlayerOne=true;
 var openPlayerTwo=true;
 var canJoin = true;
 
+//Scoring variables
 var numWins = 0;
 var numLosses = 0;
-
 var winsP1=0;
 var winsP2=0;
 var lossesP1=0;
 var lossesP2=0;
-var winnerText="";
-var winnerNumber = 0;
 
+//Choice Evariables
 var choiceOne;
 var choiceTwo;
 
+//nding condition variables
+var winnerText="";
+var winnerNumber = 0;
+
+//Timers
 var gameTimer;
 var resultsTimer;
 
+//Audio 
 var playAudio = true;
 
+/*
+ * JQuery ready, click functions
+ */
 
+ //Document ready
 $(document).ready(function(){
 	
 	$("#playerNameTextbox").val("Enter Your Name");
@@ -55,6 +69,7 @@ $(document).ready(function(){
 	$("#myModal").modal('show');
 });
 
+//Audio button image change and audio mute
 $(document).on("click", ".audioButton", function(){
 	if(playAudio){
 		$(".audioButton").removeClass("fa-pause");
@@ -71,14 +86,12 @@ $(document).on("click", ".audioButton", function(){
 	}
 });
 
-
-
-
-
+//Clear chat textbox on click
 $(document).on("click", "#playerNameTextbox", function(){
 	$(this).val("");
 });
 
+//Select portrait for player
 $(document).on("click", ".fightersPortrait", function(){
 
 	$(".fightersPortrait").removeClass("portraitBackground");
@@ -97,24 +110,26 @@ $(document).on("click", ".fightersPortrait", function(){
 	}
 });
 
+//Start game after player meets conditions
 $(document).on("click", "#modalButton", function(){
 	if(hasSelectedFighter){
 		if("Enter Your Name"!== $("#playerNameTextbox").val() && $("#playerNameTextbox").val() !== ""){
 			playerName = $("#playerNameTextbox").val();
 		}
 
-
 		if(openPlayerOne){
 			$("#playerName1").html(playerName);
 			addUser(playerName, 1, playerWarrior);
 
 		}
+
 		else if(openPlayerTwo){
 			$("#playerName2").html(playerName);
 			addUser(playerName, 2, playerWarrior);
 		}
+
 		else{
-			alert("Game Full"); // ----------- ADD CANNOT JOIN MESSAGE AT THIS TIME
+			alert("Game Full"); // ----------- ADD "CANNOT JOIN AT THIS TIME" MESSAGE
 		}
 	}
 });
@@ -137,6 +152,7 @@ $("input").keypress(function(event) {
     }
 });
 
+//Click weapon and sets player choice
 $(document).on("click", ".weaponImage", function(){
 	var itemChoice = $(this).data("item");
 	$("#textResult").empty();
@@ -148,8 +164,27 @@ $(document).on("click", ".weaponImage", function(){
 
 });
 
+/*
+ * Database functions
+ */
+
+ //Connect and Disconnect from database
+ function connectDB(name){
+	database.ref().child(".info").on("child_added", function(snapshot) {
+	  	if (snapshot.val() === true) {
+	    	writeChat(name+" has entered the game!");
+	  	} 
+	});
+}
+function disconnectDB(num){
+	database.ref().child("chat").onDisconnect().update({line: playerName+" has left the game!"});
+	database.ref().child("player").child(playerID).onDisconnect().remove();
+}
+
+//Get current players from database
 function getCurrentPlayers(){
 
+	//See what player positions are open when just joining game or a child is added 
 	database.ref().child("player").on("child_added", function(snapshot) {
 
 		 if(snapshot.val().seat === 1){
@@ -168,8 +203,14 @@ function getCurrentPlayers(){
 		 	openPlayerTwo=false;
 		 	fighterName2 = snapshot.val().fighter;
 		 }
+
+		 //Show message/fighting screen if both players are present
+		 if(!openPlayerOne && !openPlayerTwo){
+			$("#fightBox").css({"visibility":"visible"});
+		}
 	});
 
+	//When a player leaves remove character info from game
 	database.ref().child("player").on("child_removed", function(snapshot) {
 
 		var seatNum = snapshot.val().seat;
@@ -192,6 +233,8 @@ function getCurrentPlayers(){
 	});
 }
 
+
+//See what player positions are open and add the player if possible
 function addUser(currentPlayer, seatNum, warrior){
 
 	if((openPlayerOne  || openPlayerTwo) && canJoin){
@@ -238,10 +281,14 @@ function addUser(currentPlayer, seatNum, warrior){
 						}});
 			playerID=2;
 		}
+
+		//Change intro music to game music
 		$("#music_player").removeAttr("autoplay");
 		$("#music_player").removeAttr("loop");
 		changeAudio();
 		$("#music_player").on('ended', changeAudio);
+
+		//Setup player conditions
 		playerName=currentPlayer;
 		connectDB(playerName);
 		disconnectDB(playerID);
@@ -279,48 +326,42 @@ function readChat(){
 	});
 }
 
-function disconnectDB(num){
-	database.ref().child("chat").onDisconnect().update({line: playerName+" has left the game!"});
-	database.ref().child("player").child(playerID).onDisconnect().remove();
-}
-
-function connectDB(name){
-	database.ref().child(".info").on("child_added", function(snapshot) {
-	  	if (snapshot.val() === true) {
-	    	writeChat(name+" has entered the game!");
-	  	} 
-	});
-}
-
+//Clear chat when joining a game in database
 function clearChat(){
 	database.ref().child("chat").set({line:""});
 }
 
+//Sends the player choice to the database
 function playerChoice(item){
 	var tempObj =  {choice: item};
 	database.ref().child("player").child(playerID).update(tempObj);
 }
 
+//Get results from database and checks for winning condition
 function checkResult(){
 
 	database.ref().on("child_changed", function(snapshot) {
 
+		//Check to see if both players have made a choice
 		var tempBool1 = snapshot.child("1").child("choice").exists();
 		var tempBool2 = snapshot.child("2").child("choice").exists();
 
-		if(tempBool1  && tempBool2){
+		//If both choices have been made
+		if(tempBool1  && tempBool2){ 
 			var pOneChoice = snapshot.child("1").child("choice").val();
 			var pTwoChoice = snapshot.child("2").child("choice").val();
-			clearResults();
-			$("#fightBox").css({"visibility":"visible"});
+			clearResults(); // remove choice from database
+			//P1 - Rock
+			//P2 - Scissors
 			if ((pOneChoice === "rock") && (pTwoChoice === "scissors")){
-				
 				$("#playerRPS1").attr("alt", "rock").attr("width", "100").attr("src", "assets/images/left_R.gif");
 				$("#playerRPS2").attr("alt", "scissors").attr("width", "100").attr("src", "assets/images/right_S.gif");
 				winnerText=$("#playerName1").html()+" Wins!";
 				winnerNumber =1;
 				results(1);
 			}
+			//P1 - Rock
+			//P2 - Paper
 			else if ((pOneChoice === "rock") && (pTwoChoice === "paper")){
 				$("#playerRPS1").attr("alt", "rock").attr("width", "100").attr("src", "assets/images/left_R.gif");
 				$("#playerRPS2").attr("alt", "paper").attr("width", "100").attr("src", "assets/images/right_P.gif");
@@ -328,6 +369,8 @@ function checkResult(){
 				winnerNumber =2;
 				results(2);
 			}
+			//P1 - Scissors
+			//P2 - Rock
 			else if ((pOneChoice === "scissors") && (pTwoChoice === "rock")){
 				$("#playerRPS1").attr("alt", "scissors").attr("width", "100").attr("src", "assets/images/left_S.gif");
 				$("#playerRPS2").attr("alt", "rock").attr("width", "100").attr("src", "assets/images/right_R.gif");
@@ -335,6 +378,8 @@ function checkResult(){
 				winnerNumber =2;
 				results(2);
 			}
+			//P1 - Scissors
+			//P2 - Paper
 			else if ((pOneChoice === "scissors") && (pTwoChoice === "paper")){
 				$("#playerRPS1").attr("alt", "scissors").attr("width", "100").attr("src", "assets/images/left_S.gif");
 				$("#playerRPS2").attr("alt", "paper").attr("width", "100").attr("src", "assets/images/right_P.gif");
@@ -342,6 +387,8 @@ function checkResult(){
 				winnerNumber =1;
 				results(1);
 			}
+			//P1 - Paper
+			//P2 - Rock
 			else if ((pOneChoice === "paper") && (pTwoChoice === "rock")){
 				$("#playerRPS1").attr("alt", "paper").attr("width", "100").attr("src", "assets/images/left_P.gif");
 				$("#playerRPS2").attr("alt", "rock").attr("width", "100").attr("src", "assets/images/right_R.gif");
@@ -349,6 +396,8 @@ function checkResult(){
 				winnerNumber =1;
 				results(1);
 			}
+			//P1 - Paper
+			//P2 - Scissors
 			else if ((pOneChoice === "paper") && (pTwoChoice === "scissors")){
 				$("#playerRPS1").attr("alt", "paper").attr("width", "100").attr("src", "assets/images/left_P.gif");
 				$("#playerRPS2").attr("alt", "scissors").attr("width", "100").attr("src", "assets/images/right_S.gif");
@@ -356,6 +405,7 @@ function checkResult(){
 				winnerNumber =2;
 				results(2);
 			}
+			//Tie - Paper
 			else if ((pOneChoice === pTwoChoice) && (pTwoChoice === "paper")){
 				$("#playerRPS1").attr("alt", "paper").attr("width", "100").attr("src", "assets/images/left_P.gif");
 				$("#playerRPS2").attr("alt", "paper").attr("width", "100").attr("src", "assets/images/right_P.gif");
@@ -363,6 +413,7 @@ function checkResult(){
 				winnerNumber =0;
 				results(0);
 			}
+			//Tie - Scissors
 			else if ((pOneChoice === pTwoChoice) && (pTwoChoice === "scissors")){
 				$("#playerRPS1").attr("alt", "scissors").attr("width", "100").attr("src", "assets/images/left_S.gif");
 				$("#playerRPS2").attr("alt", "scissors").attr("width", "100").attr("src", "assets/images/right_S.gif");
@@ -370,6 +421,7 @@ function checkResult(){
 				winnerNumber =0;
 				results(0);
 			}
+			//Tie - Rock
 			else if ((pOneChoice === pTwoChoice) && (pTwoChoice === "rock")){
 				$("#playerRPS1").attr("alt", "rock").attr("width", "100").attr("src", "assets/images/left_R.gif");
 				$("#playerRPS2").attr("alt", "rock").attr("width", "100").attr("src", "assets/images/right_R.gif");
@@ -381,6 +433,11 @@ function checkResult(){
 	});	
 }
 
+/*
+ * Local game functions
+ */
+
+//Update local win/losses based on game results
 function results(playerNum){
 	console.log(numWins, numLosses);
 	if(playerNum === playerID){
@@ -390,15 +447,17 @@ function results(playerNum){
 
 		numLosses+=1;
 	}
-	updateScoreDB();
-	newGameTimer();
+	updateScoreDB();  //Update score in database
+	newGameTimer();  //Timer to show results
 }
 
+//Remove choices from database
 function clearResults(){
 	database.ref().child("player").child("1").child("choice").remove();
 	database.ref().child("player").child("2").child("choice").remove();
 }
 
+//Update score in database
 function updateScoreDB(){
 	if(playerID!==0){
 		var tempObj =  {wins: numWins};
@@ -408,6 +467,7 @@ function updateScoreDB(){
 	}
 }
 
+//Get both player scores from database
 function getScore(){
 
 	database.ref().child("player").on("child_changed", function(snapshot) {
@@ -423,6 +483,7 @@ function getScore(){
 	});
 }
 
+//Timers to show results for a limited time
 function newGameTimer(){
 
 	$("#textResult").empty();
@@ -433,7 +494,7 @@ function newGameTimer(){
 	}
 	,2000);
 
-
+	//resets results screen after timer expires
 	gameTimer = setTimeout(function() {
 		//Replacing SRC. If not, gifs will not reload animation.
 		$("#playerRPS1").attr("src", "assets/images/characters/animation/empty.png");
@@ -451,6 +512,7 @@ function newGameTimer(){
 	, 5000);
 }
 
+//Change player locations boolean if a player leaves
 function leaveChair(seatNum){
 	if(seatNum===1){
 		openPlayerOne=true;
@@ -460,6 +522,7 @@ function leaveChair(seatNum){
 	}
 }
 
+//Show weapons when entering game and new round
 function showWeapons(){
 		$("#textResult").append("<p class=\"weaponText\">Choose Your Weapon</p>");
 	  	$("#weaponHolderPlayer"+playerID).empty();
@@ -471,6 +534,7 @@ function showWeapons(){
 		$("#weaponHolderPlayer"+playerID).css({"visibility":"visible"});
 }
 
+//Shows results after newGameTimer() expires
 function resultsDelay(){
 	$("#textResult").append("<p class=\"endText\">"+winnerText+"</p>");
 	var ratioP1 = (lossesP1/(lossesP1+winsP1))*100;
@@ -482,13 +546,19 @@ function resultsDelay(){
 	$("#scorePlayer2").html("Wins: "+winsP2+" Losses: "+lossesP2);
 }
 
+/*
+ * Audio functions
+ */
+
+//Change music (randomly) and play if not muted
 function changeAudio() {  
 	$("#music_player").attr("src", MUSIC_DIR+MUSIC_LIST[Math.ceil(Math.random()*MUSIC_LIST.length)-1]);
 	if(playAudio){
 		document.getElementById("music_player").play();
 	}
-	
 }
+
+//Mute sound
 function mute(sound){
 	if(sound){
 		document.getElementById("music_player").play();
@@ -498,9 +568,12 @@ function mute(sound){
 	}
 }
 
+//Show audio control after player enters (for better presentation)
 function showAudioControls(){
  	$("#audioBox").css({"visibility":"visible"});
 }
+
+//Play the winner audio phrase if not muted
 function audioWinner() {  
 	var winner;
 	if(winnerNumber === 1){
